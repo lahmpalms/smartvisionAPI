2f4639ba77c752ba8270a4379d2e1700ee38bd59
lahmpalms <lahmpalms@lahmpalmss-MacBook-Pro.local>
2023-11-16 11:14:40 +0700

    accept image and fix response

--- people_detect_api/api.py
+++ people_detect_api/api.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import base64
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
@@ -6,24 +7,44 @@ from peoplecountv8 import main  # Import your detection function
 
 app = FastAPI()
 
+
 @app.post("/detect-people/")
 async def detect_people(image: UploadFile):
-    # Save the uploaded image file
-    with open("input_image.jpg", "wb") as f:
-        f.write(image.file.read())
+    try:
+        # Save the uploaded image file
+        file_path = "input_image.jpg"
+        with open(file_path, "wb") as f:
+            f.write(image.file.read())
 
-    # Call your YOLOv8 detection function
-    output_image_path = main("input_image.jpg")  # Modify the detection function
+        # Call your YOLOv8 detection function asynchronously
+        output_image_path = main(file_path)
+        coordinate = []
+        for index, box in enumerate(output_image_path["results"].boxes):
+            data = {
+                "people_id": index,
+                "people_cor": box.xyxy.tolist(),
+                "class_id": box.cls.item(),
+                "confidence": box.conf.item()
+            }
+            coordinate.append(data)
+        if output_image_path["latest_image_path"]:
+            # Convert the output image to base64
+            with open(output_image_path["latest_image_path"], "rb") as image_file:
+                image_base64 = base64.b64encode(
+                    image_file.read()).decode("utf-8")
+                if os.path.exists(output_image_path["latest_image_path"]):
+                    os.remove(output_image_path["latest_image_path"])
+                else:
+                    print(f"File does not exist.")
 
-    if output_image_path:
-        # Convert the output image to base64
-        with open(output_image_path, "rb") as image_file:
-            image_base64 = base64.b64encode(image_file.read()).decode("utf-8")
-
-        return JSONResponse(content={"image_base64": image_base64})
-    else:
-        return JSONResponse(content={"error": "No valid 'predictX' directories found."})
-
+            return JSONResponse(content={"data": {
+                "base64_image": image_base64, "coordinate_data": coordinate},
+                "code": 200,
+                "message": "ok", })
+        else:
+            return JSONResponse(content={"error": "No valid 'predict' directories found."})
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)})
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
--- people_detect_api/peoplecountv8.py
+++ people_detect_api/peoplecountv8.py
@@ -1,53 +1,53 @@
+import os
 from ultralytics import YOLO
-import cv2
-import os
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
-import os
 
 def get_latest_output_image():
-    # Define the directory where YOLOv8 saves the output images
     output_directory = 'runs/detect'
-
-    # List all directories (each corresponding to a run)
     run_directories = os.listdir(output_directory)
-
-    # Filter for directories that start with 'predict' (assuming this pattern)
-    predict_directories = [d for d in run_directories if d.startswith('predict')]
+    predict_directories = [
+        d for d in run_directories if d.startswith('predict')]
 
     if not predict_directories:
-        return None  # No 'predictX' directories found
+        return None  # No 'predict' directories found
 
-    # Sort the predict directories based on the numeric part and get the latest
-    latest_predict_directory = max(predict_directories, key=lambda d: int(d[len('predict'):]) if d[len('predict'):] else -1)
-
-    # Construct the path to the latest output image file (assuming it's named 'output.jpg')
-    latest_output_image = os.path.join(output_directory, latest_predict_directory, 'input_image.jpg')
+    latest_predict_directory = max(predict_directories, key=lambda d: int(
+        d[len('predict'):]) if d[len('predict'):] else -1)
+    latest_output_image = os.path.join(
+        output_directory, latest_predict_directory, 'input_image.jpg')
 
     return latest_output_image
 
-# Usage
-latest_image_path = get_latest_output_image()
-
-if latest_image_path:
-    print("Latest Output Image Path:", latest_image_path)
-else:
-    print("No 'predictX' directories found.")
-
-
-
 
 def main(input_image_path):
-    # Load the YOLOv8 model
-    model = YOLO('yolov8s.pt')
+    try:
+        model = YOLO('yolov8s.pt')
 
-    # Perform YOLOv8 object detection
-    results = model.predict(input_image_path, save=True)
+        # Perform YOLOv8 object detection
+        results = model.predict(input_image_path, save=True, classes=0)
 
-    # Save the processed image to an output file
-    latest_image_path = get_latest_output_image()
+        logging.info(f"Results saved in: {results[0].save_dir}")
 
-    return latest_image_path
+        # Save the processed image to an output file
+        latest_image_path = get_latest_output_image()
 
-if __name__ == '__main__':
-    main("/home/nont/entech/shutterstock_721660498.jpg")
+        data = {
+            "results": results[0],
+            "latest_image_path": latest_image_path
+        }
+
+        if latest_image_path:
+            logging.info(f"Latest Output Image Path: {latest_image_path}")
+        else:
+            logging.warning("No 'predict' directories found.")
+
+        # Delete the input image and the YOLOv8 output directory
+        os.remove(input_image_path)
+
+        return data
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+        return None
